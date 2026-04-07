6b001b481e17ec2ad1a3e7ef087ada7812e08ad8
njit_77 <512424964@qq.com>
2026-04-07 20:22:41 +0800

    1

--- log/log_export.h
+++ log/log_export.h
@@ -1,15 +1,15 @@
-#pragma once
+﻿#pragma once
 
 #include<Windows.h>
 
 #ifdef LOG_EXPORTS
-#define LOG_API __declspec(dllexport)
+#define LOG_API __declspec(dllexport) 
 #else
 #define LOG_API __declspec(dllimport)
 #endif
 
 
-LOG_API void SetLogFileName(const char* log_name, HMODULE module, bool console);
+LOG_API void __cdecl SetLogFileName(const char* log_name, HMODULE module, bool console);
 
 enum level_enum
 {
@@ -21,11 +21,11 @@ enum level_enum
 	critical = 5,
 	off = 6,
 };
-LOG_API void SetLogLevel(int level);
+LOG_API void __cdecl SetLogLevel(int level);
 
-LOG_API void LogTrace(const char* format, ...);
-LOG_API void LogDebug(const char* format, ...);
-LOG_API void LogInfo(const char* format, ...);
-LOG_API void LogWarn(const char* format, ...);
-LOG_API void LogError(const char* format, ...);
-LOG_API void LogCritical(const char* format, ...);
+LOG_API void __cdecl LogTrace(const char* format, ...);
+LOG_API void __cdecl LogDebug(const char* format, ...);
+LOG_API void __cdecl LogInfo(const char* format, ...);
+LOG_API void __cdecl LogWarn(const char* format, ...);
+LOG_API void __cdecl LogError(const char* format, ...);
+LOG_API void __cdecl LogCritical(const char* format, ...);
